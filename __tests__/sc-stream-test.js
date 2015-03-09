1287aab6df4d7ac4ae947346d3191ca05750fad8
troy betz <troybetz@gmail.com>
2015-03-09 17:52:45 -0500

    rewrite tests using es6

--- __tests__/sc-stream-test.js
+++ __tests__/sc-stream-test.js
@@ -1,23 +1,21 @@
 jest.dontMock('../');
 
-describe('SCStream', function() {
-  var requiresdk = require('require-sdk');
-  var SCStream = require('../');
-  var sdkMock;
+describe('SCStream', () => {
+  const requiresdk = require('require-sdk');
+  const SCStream = require('../');
 
-  beforeEach(function() {
+  let sdkMock;
+  let scstream;
+
+  beforeEach(() => {
 
     /**
      * Mock out API loading util
      */
 
-    sdkMock = jest.genMockFunction().mockImplementation(function(cb) {
-      return cb();
-    });
-    
-    requiresdk.mockImplementation(function(url, cb) {
-      return sdkMock;
-    });
+    sdkMock = jest.genMockFunction().mockImplementation((cb) => cb());
+
+    requiresdk.mockImplementation(() => sdkMock);
 
     /**
      * Mock out SoundCloud API
@@ -26,78 +24,62 @@ describe('SCStream', function() {
     window.SC = {
       initialize: jest.genMockFunction(),
 
-      get: jest.genMockFunction().mockImplementation(function(u, t, cb) {
+      get: jest.genMockFunction().mockImplementation((u, t, cb) => {
         cb({
           stream_url: 'a streaming url'
         });
       }),
 
-      stream: jest.genMockFunction().mockImplementation(function(url, cb) {
+      stream: jest.genMockFunction().mockImplementation((url, cb) => {
         cb('a stream');
       })
     };
+
+    scstream = new SCStream('sc-client-id');
   });
 
-  it('should raise an error if the SoundCloud API fails to load', function() {
-    sdkMock.mockImplementation(function(cb) {
-      cb('error');
-    });
-
-    expect((function() {
-      new SCStream();
-    })).toThrow('SoundCloud API failed to load');
+  it('should raise an error if the SoundCloud API fails to load', () => {
+    sdkMock.mockImplementation((cb) => cb('error'));
+    expect((() => { new SCStream();})).toThrow('SoundCloud API failed to load');
   });
 
-  it('should load the SoundCloud API when initialized', function() {
-    var scstream = new SCStream();
-
+  it('should load the SoundCloud API when initialized', () => {
     expect(requiresdk.mock.calls[0][0]).toBe('http://connect.soundcloud.com/sdk.js');
     expect(requiresdk.mock.calls[0][1]).toBe('SC');
     expect(sdkMock.mock.calls.length).toBe(1);
   });
 
 
-  it('should authenticate a new soundcloud client', function() {
-    var scstream = new SCStream('sc-client-id');
-    
+  it('should authenticate a new soundcloud client', () => {
     expect(window.SC.initialize.mock.calls[0][0].client_id).toBe('sc-client-id');
   });
 
-  it('should retrieve track information', function() {
-    var scstream = new SCStream('sc-client-id');
-
+  it('should retrieve track information', () => {
     scstream.getTrackInfo('https://soundcloud.com/baauer/one-touch');
-
     expect(window.SC.get.mock.calls[0][1].url).toBe('https://soundcloud.com/baauer/one-touch');
   });
 
-  it('should create a new audio stream', function() {
-    var scstream = new SCStream('sc-client-id');
-    
-    scstream.createStream('a streaming url');    
+  it('should create a new audio stream', () => {
+    scstream.createStream('a streaming url');
     expect(window.SC.stream.mock.calls[0][0]).toBe('a streaming url');
   });
 
-  pit('should retrieve track info and create a stream', function() {
-    var scstream = new SCStream('sc-client-id');
-
+  pit('should retrieve track info and create a stream', () => {
     return scstream.stream('https://soundcloud.com/baauer/one-touch')
-      .then(function(track) {
+      .then((track) => {
         expect(track.data).toEqual({stream_url: 'a streaming url'});
         expect(track.stream).toBe('a stream');
       });
   });
 
-  pit('should be able to stagger loading track info & stream', function() {
-    var scstream = new SCStream('sc-client-id');
-
+  pit('should be able to stagger loading track info & stream', () => {
     return scstream.staggerStream('https://soundcloud.com/baauer/one-touch')
-      .then(function(track) {
+      .then((track) => {
         expect(track.data).toEqual({stream_url: 'a streaming url'});
         expect(track.stream.then).toBeDefined(); // track.stream is a promise
         return track.stream;
       })
-      .then(function(stream) {
+      .then((stream) => {
         expect(stream).toBe('a stream');
       });
   });
